a348c213b740739e7149443d047d527fc8c515dd
wilaxx <wilaxx59@gmail.com>
2023-04-25 18:09:41 +0200

    ajout des variables random

--- skills.js
+++ skills.js
@@ -2,3 +2,6 @@ const known = ['HTML5', 'CSS3', 'Javascript', 'Git', 'GitHub', 'C(basics)', 'Bas
 const improvements = ['GitLab CI/CD', 'Docker', 'Docker Compose', 'Test unitaire', 'AWS', 'SSL', 'VLAN', 'bash'];
 const must = ['Kubernetes', 'DevOps', 'TerraForm', 'Docker', 'Rust', 'Python', 'PHP', 'SQL', 'Powershell'];
 
+let i = Math.floor(Math.random() * (known.length + 1));
+let j = Math.floor(Math.random() * (improvements.length + 1));
+let k = Math.floor(Math.random() * (must.length + 1));
