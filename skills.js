c9f16aa827198085ffd4afe908c2579ed659b1a7
wilaxx <wilaxx59@gmail.com>
2023-04-25 18:02:49 +0200

    ajout des tableaux

---
+++ skills.js
@@ -0,0 +1,4 @@
+const known = ['HTML5', 'CSS3', 'Javascript', 'Git', 'GitHub', 'C(basics)', 'Bash'];
+const improvements = ['GitLab CI/CD', 'Docker', 'Docker Compose', 'Test unitaire', 'AWS', 'SSL', 'VLAN', 'bash'];
+const must = ['Kubernetes', 'DevOps', 'TerraForm', 'Docker', 'Rust', 'Python', 'PHP', 'SQL', 'Powershell'];
+
